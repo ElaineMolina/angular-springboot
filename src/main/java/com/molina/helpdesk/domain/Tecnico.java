e0ed62c8d2018688aaacd4f92ed1e8a12d6a33b0
ElaineMolina <elainemolina10@gmail.com>
2023-03-30 18:49:56 -0300

    anotações JPA e classes de domínio

--- src/main/java/com/molina/helpdesk/domain/Tecnico.java
+++ src/main/java/com/molina/helpdesk/domain/Tecnico.java
@@ -1,18 +1,27 @@
 package com.molina.helpdesk.domain;
 
+import com.molina.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
     public List<Chamado> getChamados() {

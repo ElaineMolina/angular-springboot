d291299915b885fe97d807d71e4ca78da5321e79
ElaineMolina <elainemolina10@gmail.com>
2023-04-24 16:24:35 -0300

    endpoint delete para Tecnico

--- src/main/java/com/molina/helpdesk/services/TecnicoService.java
+++ src/main/java/com/molina/helpdesk/services/TecnicoService.java
@@ -44,6 +44,15 @@ public class TecnicoService {
         oldObj = new Tecnico(objDTO);
         return repository.save(oldObj);
     }
+
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if(obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+        }else {
+            repository.deleteById(id);
+        }
+    }
     private void validaPorCpfEEmail(TecnicoDTO objDTO) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
@@ -54,6 +63,7 @@ public class TecnicoService {
             throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
         }
     }
+
 
 
 }

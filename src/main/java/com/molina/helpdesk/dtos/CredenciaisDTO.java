e01228f83d4b047d3285662b7747b747c5891f92
ElaineMolina <elainemolina10@gmai.com>
2023-05-24 19:00:23 -0300

    Classe CredenciaisDTO e Inclusão das propriedades de JWT

---
+++ src/main/java/com/molina/helpdesk/dtos/CredenciaisDTO.java
@@ -0,0 +1,22 @@
+package com.molina.helpdesk.dtos;
+
+public class CredenciaisDTO {
+    private String emai;
+    private String senha;
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
